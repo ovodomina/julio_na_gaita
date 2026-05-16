8e5d41d69d3471a7d673e94d4c8155b0f77ab9b0
ovodomina <yuri.costa4@fatec.sp.gov.br>
2026-05-16 11:46:53 -0300

    Update validador.py

--- src/validador.py
+++ src/validador.py
@@ -1,5 +1,5 @@
 def validar_email(email: str) -> bool:
-    unused_var = "F841" # Variável local criada mas nunca usada!
+    #unused_var = "F841" # Variável local criada mas nunca usada!
     """Verifica se existe um '@' e um '.' no e-mail."""
     return "@" in email and "." in email
 def eh_maior_de_idade(idade: int) -> bool:
