8d7211389e5c4da24c1d090954e767eb7859ab1b
ovodomina <yuri.costa4@fatec.sp.gov.br>
2026-05-16 11:04:58 -0300

    [MEUS DEUS] Varias coisas

---
+++ src/validador.py
@@ -0,0 +1,13 @@
+import os # Biblioteca importada, mas nunca usada (O Ruff vai reclamar!)
+import sys # Biblioteca EXTRA adicionada para o Ruff acusar erro F401
+import math # Biblioteca EXTRA adicionada para o Ruff acusar erro F401
+
+def validar_email(email: str) -> bool:
+    unused_var = "F841" # Variável local criada mas nunca usada!
+    """Verifica se existe um '@' e um '.' no e-mail."""
+    return "@" in email and "." in email
+def eh_maior_de_idade(idade: int) -> bool:
+    """Verifica se o usuário tem 18 anos ou mais."""
+    if idade < 0:
+        raise ValueError("Idade não pode ser negativa")
+    return idade >= 18
